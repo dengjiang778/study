f8b16745659ddaf5d4cfd23759a9dc46e158ab84
dengjiang <1640778036@qq.com>
2020-05-25 10:01:17 +0800

    Revert "ss"
    This reverts commit f7f626f2a1ecd26f9c562da1ecce0e659aa94cbf.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,13 +1,17 @@
 package com.example.demo.model;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Transient;
+import java.util.Collection;
 import java.util.Date;
 
 /**
  * User
  */
-public class User {
+public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     public User() {
@@ -122,11 +126,32 @@ public class User {
 
     private String token;
 
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
     public boolean isAccountNonExpired() {
         return true;
     }
 
     public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
         return true;
     }
 
@@ -338,12 +363,14 @@ public class User {
         this.token = token;
     }
 
+    @Override
     public String getUsername() {
-        return username;
+        return this.username;
     }
 
+    @Override
     public String getPassword() {
-        return password;
+        return this.password;
     }
 
     @Override
